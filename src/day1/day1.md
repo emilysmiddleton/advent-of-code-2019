280a7cfac2c6a281ce4522c9040a58c262e06824
middletone <emilysmiddleton@gmail.com>
2020-01-04 15:45:03 +0000

    Parse the input - convert to a number

---
+++ src/day1/day1.md
@@ -0,0 +1,13 @@
+# Day 1
+
+See [part 1](problem-part-1.md), or [the original online](https://adventofcode.com/2019/day/1/).
+
+## Parsing the input
+
+Each line in the input file represents the mass of a module. Little parsing is needed here beyond converting
+from a string to a number. In TypeScript we use `Number.parseInt`
+
+## Fuel required
+
+> Fuel required to launch a given module is based on its mass. 
+> Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
--- src/day1/main.ts
+++ src/day1/main.ts
@@ -1,14 +1,14 @@
 import * as Logger from 'bunyan';
 
-export function parse(rawInputs: string[], _log: Logger): string[] {
-    return rawInputs;
+export function parse(rawInputs: string[], _log: Logger): number[] {
+    return rawInputs.map(input => Number.parseInt(input, 10));
 }
 
-export function run1(input: string[], _log: Logger): string {
+export function run1(input: number[], _log: Logger): number {
     return input[0];
 }
 
-export function run2(input: string[], _log: Logger): string {
+export function run2(input: number[], _log: Logger): number {
     return input[0];
 
 }
