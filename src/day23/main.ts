af8cd9431c15584a80c987fe7cd07e1fcb028d26
middletone <emilysmiddleton@gmail.com>
2020-01-04 15:18:12 +0000

    Placeholders for other days

---
+++ src/day23/main.ts
@@ -0,0 +1,17 @@
+import * as Logger from 'bunyan';
+
+export function parse(rawInputs: string[], _log: Logger): string[] {
+    return rawInputs;
+}
+
+export function run1(input: string[], _log: Logger): string {
+    return input[0];
+}
+
+export function run2(input: string[], _log: Logger): string {
+    return input[0];
+
+}
+
+
+
