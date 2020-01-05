6c5a553571ec3dc6cc991a0bd3a13e86a97639d3
middletone <emilysmiddleton@gmail.com>
2020-01-05 15:54:37 +0000

    Parse the input to get the range

--- src/day4/day4.md
+++ src/day4/day4.md
@@ -2,4 +2,15 @@
 
 See [part 1](problem.md), or [the original online](https://adventofcode.com/2019/day/4/).
 
+## Parsing the input
 
+The input is a range separated by a hypen; split on the hyphen and convert each part to a number.
+
+I've chosen to define a range type.
+
+```
+{
+    from: number
+    to: number
+}
+```
--- src/day4/main.ts
+++ src/day4/main.ts
@@ -1,15 +1,20 @@
 import * as Logger from 'bunyan';
+import { Range } from './types';
 
-export function parse(rawInputs: string[], _log: Logger): string[] {
-    return rawInputs;
+export function parse(rawInputs: string[], _log: Logger): Range {
+    const parts = rawInputs[0].split('-');
+    return {
+        from: Number.parseInt(parts[0], 10),
+        to: Number.parseInt(parts[1], 10)
+    };
 }
 
-export function run1(input: string[], _log: Logger): string {
-    return input[0];
+export function run1(input: Range, _log: Logger): number {
+    return input.from;
 }
 
-export function run2(input: string[], _log: Logger): string {
-    return input[0];
+export function run2(input: Range, _log: Logger): number {
+    return input.from;
 
 }
 
